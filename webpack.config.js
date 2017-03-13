7a6431c0bf630b9b983a40c1b2f66961723b17d3
Joppe Aarts <joppe@zicht.nl>
2017-03-13 09:43:11 +0100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,84 @@
+const webpack = require('webpack');
 const path = require('path');
 
+/**
+ * Webpack configuration
+ *
+ * See: http://webpack.github.io/docs/configuration.html#cli
+ */
 const config = {
-    entry: {
-        'dist/pad': './src/pad.ts',
-        'dist/repeat': './src/repeat.ts',
-        'dist/reverse': './src/reverse.ts'
-    },
+    entry: {},
 
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname)
     },
 
+    /**
+     * Source map for Karma from the help of karma-sourcemap-loader &  karma-webpack
+     *
+     * Do not change, leave as is or it wont work.
+     * See: https://github.com/webpack/karma-webpack#source-maps
+     */
+    devtool: 'source-map',
+
+    /**
+     * Options affecting the resolving of modules.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#resolve
+     */
     resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+        /**
+         * An array of extensions that should be used to resolve modules.
+         *
+         * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
+         */
+        extensions: ['.ts', '.tsx', '.js']
     },
 
+    /**
+     * Options affecting the normal modules.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#module
+     *
+     * 'use:' revered back to 'loader:' as a temp. workaround for #1188
+     * See: https://github.com/AngularClass/angular2-webpack-starter/issues/1188#issuecomment-262872034
+     */
     module: {
         rules: [
+            /**
+             * Source map loader support for *.js files
+             * Extracts SourceMaps for source files that as added as sourceMappingURL comment.
+             *
+             * See: https://github.com/webpack/source-map-loader
+             */
             {
-                test: /\.ts$/,
                 enforce: 'pre',
-                loader: 'tslint-loader',
-                options: {
-                    configFile: './node_modules/zicht-tslint/tslint.json',
-
-                    tsConfigFile: './tsconfig.json'
-                }
-            },
-
-            {
                 test: /\.js$/,
-                use: ['source-map-loader'],
-                enforce: 'pre'
+                loader: 'source-map-loader'
             },
 
+            /**
+             * Typescript loader support for .ts
+             */
             {
-                test: /\.tsx?$/,
+                test: /\.(t|j)sx?$/,
                 loader: 'ts-loader'
             }
         ]
     },
 
-    devtool: 'source-map'
+    /**
+     * Add additional plugins to the compiler.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#plugins
+     */
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: false,
+            sourceMap: true
+        })
+    ]
 };
 
 module.exports = config;
