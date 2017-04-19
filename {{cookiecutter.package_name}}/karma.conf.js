71e3fd9c095fb2ec66121d731289d5182a65a4e3
Joppe Aarts <joppe@zicht.nl>
2017-04-19 10:10:20 +0200

    moved karma config and package.json to target dir use karma-typescript instead of karma-webpack, is more clear use headless Chrome instead of PhantomJS

---
+++ {{cookiecutter.package_name}}/karma.conf.js
@@ -0,0 +1,72 @@
+/*global module*/
+
+module.exports = function (config) {
+    const configuration = {
+        frameworks: ['jasmine', 'karma-typescript'],
+
+        files: [
+            {
+                pattern: 'src/**/*.ts'
+            },
+            {
+                pattern: 'test/**/*.ts'
+            }
+        ],
+
+        preprocessors: {
+            'src/**/*.ts': ['karma-typescript'],
+            'test/**/*.ts': ['karma-typescript']
+        },
+
+        reporters: [
+            'progress',
+            'karma-typescript'
+        ],
+
+        karmaTypescriptConfig: {
+            reports: {
+                clover: {
+                    directory: 'coverage',
+                    subdirectory: 'clover',
+                    filename: 'coverage.xml'
+                },
+                'html': {
+                    directory: 'coverage',
+                    subdirectory: 'html',
+                    filename: 'coverage'
+                }
+            },
+            compilerOptions: {
+                target: 'es6'
+            }
+        },
+
+        port: 9876,
+
+        colors: true,
+
+        logLevel: config.LOG_WARN,
+
+        autoWatch: false,
+
+        browsers: [
+            'ChromeNoSandboxHeadless'
+        ],
+
+        customLaunchers: {
+            ChromeNoSandboxHeadless: {
+                base: 'Chrome',
+                flags: [
+                    '--no-sandbox',
+                    '--headless',
+                    '--disable-gpu',
+                    ' --remote-debugging-port=9222'
+                ]
+            }
+        },
+
+        singleRun: true
+    };
+
+    config.set(configuration);
+};
